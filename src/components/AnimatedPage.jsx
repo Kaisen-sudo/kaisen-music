41e1df2ab3ac9f818b8fc9a84c4c6ee63a957b7b
KAISEN <player711025@gmail.com>
2026-08-07 15:15:26 -0400

    Update AnimatedPage.jsx

--- src/components/AnimatedPage.jsx
+++ src/components/AnimatedPage.jsx
@@ -1,14 +1,1 @@
 import { motion } from 'framer-motion';
-
-export default function AnimatedPage({ children }) {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -30 }}
-      transition={{ duration: 0.4 }}
-    >
-      {children}
-    </motion.div>
-  );
-}
