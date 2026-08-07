8ae378e77ec6fdc500f43ed97da116ebf1eb7337
KAISEN <player711025@gmail.com>
2026-08-07 15:13:00 -0400

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,16 +1,3 @@
-import { useTheme } from '../context/ThemeContext';
+import { useTheme } from '../context/ThemeContext.jsx';
 import sunIcon from '../assets/icons/sun.svg';
 import moonIcon from '../assets/icons/moon.svg';
-
-export default function ThemeToggle() {
-  const { dark, toggleTheme } = useTheme();
-  return (
-    <button
-      onClick={toggleTheme}
-      className="p-2 rounded-full glass hover:scale-110 transition-transform"
-      aria-label="Changer le thème"
-    >
-      <img src={dark ? sunIcon : moonIcon} alt="" className="w-5 h-5" />
-    </button>
-  );
-}
